6b92076be7d77d4efb8cc7fa7275108c0dccb2a6
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-10-20 20:25:02 -0400

    preloader skeleton

--- UtilitiesJS/preloader.js
+++ UtilitiesJS/preloader.js
@@ -1,2 +1,61 @@
 ///My code
 
+
+$(document).ready(function (){
+    
+$("#city-button").click(function (event) {
+    event.preventDefault();
+    
+
+    var div1=$("<div class='preloader-wrapper big active'>");
+    var div2=$("<div class='spinner-layer spinner-blue'>");
+    var div3=$("<div class='circle-clipper left'>");
+    var div4=$("<div class='circle'>");
+    var div5=$("<div class='gap-patch'>");
+    var div6=$("<div class='circle-clipper right'>");
+    var div7=$("<div class='circle'>");
+    
+    
+    var div8=$("<div class='spinner-layer spinner-red'>");
+    var div9=$("<div class='circle-clipper right'>");
+    var div10=$("<div class='circle-clipper left'>");
+    var div11=$("<div class='circle'>");
+    var div12=$("<div class='gap-patch'>");
+    var div13=$("<div class='circle'>");
+    var div14=$("<div class='circle-clipper right'>");
+    var div15=$("<div class='circle'>");
+    
+    
+    
+    var div16=$("<div class='spinner-layer spinner-yellow'>");
+    var div17=$("<div class='circle-clipper left'>");
+    var div18=$("<div class='circle'>");
+    var div19=$("<div class='gap-patch'>");
+    var div20=$("<div class='circle'>");
+    var div21=$("<div class='circle-clipper right'>");
+    var div22=$("<div class='circle'>");
+    
+    
+    var div23=$("<div class='spinner-layer spinner-green'>");
+    var div24=$("<div class='circle-clipper left'>");
+    var div25=$("<div class='circle'>");
+    var div26=$("<div class='gap-patch'>");
+    var div27=$("<div class='circle'>");
+    var div28=$("<div class='circle-clipper right'>");
+    var div29=$("<div class='circle'>");
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+});
