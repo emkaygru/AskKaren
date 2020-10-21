7038e8554b04615f16c5da57a842452e34ec02b9
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-10-20 21:35:52 -0400

    Updated JS, added list of suggested cities upon submitting - clicking on one of three states will bring up the results

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var adviceurl = "https://cors-anywhere.herokuapp.com/https://badadvice.rest/api/random";
 var zomapikey = "eca5d70af5955828b5479ffeed90193f";
 var cityname;
+var cityid;
+var cityarray = [];
 
 $.ajax({
     url: adviceurl,
@@ -27,27 +29,44 @@ async function zomatoDisplay() {
         url: zomatocity,
         method: "GET"
     }).then(function (response) {
-        var cityid = response.location_suggestions[0].id;
         console.log(response)
-        $("#city-name").text(response.location_suggestions[0].name);
-        var zomatourl = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityid + "&entity_type=city&count=5&sort=rating&order=asc&apikey=" + zomapikey;
-        $.ajax({
-            url: zomatourl,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            // PULL INFO FOR FIRST RESTAURANT
-            $("#res-name0").text(response.restaurants[0].restaurant.name);
-            $("#res-address0").text(response.restaurants[0].restaurant.location.address);
-            $("#res-rating0").text("Rating: " + response.restaurants[0].restaurant.user_rating.aggregate_rating + " (" + response.restaurants[0].restaurant.user_rating.rating_text + ")");
-            // PULL INFO FOR SECOND RESTAURANT
-            $("#res-name1").text(response.restaurants[1].restaurant.name);
-            $("#res-address1").text(response.restaurants[1].restaurant.location.address);
-            $("#res-rating1").text("Rating: " + response.restaurants[1].restaurant.user_rating.aggregate_rating + " (" + response.restaurants[1].restaurant.user_rating.rating_text + ")");
-            // PULL INFO FOR THIRD RESTAURANT
-            $("#res-name2").text(response.restaurants[2].restaurant.name);
-            $("#res-address2").text(response.restaurants[2].restaurant.location.address);
-            $("#res-rating2").text("Rating: " + response.restaurants[2].restaurant.user_rating.aggregate_rating + " (" + response.restaurants[2].restaurant.user_rating.rating_text + ")");
-        })
+        $("#city0").attr("data-name", response.location_suggestions[0].id);
+        $("#city0").text(response.location_suggestions[0].name);
+        $("#city1").attr("data-name", response.location_suggestions[1].id);
+        $("#city1").text(response.location_suggestions[1].name);
+        $("#city2").attr("data-name", response.location_suggestions[2].id);
+        $("#city2").text(response.location_suggestions[2].name);
     })
 }
+
+$(".cities").click(function () {
+    $("#city0").addClass("hide");
+    $("#city1").addClass("hide");
+    $("#city2").addClass("hide");
+    cityid = $(this).attr("data-name");
+    var zomatourl = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityid + "&entity_type=city&count=5&sort=rating&order=asc&apikey=" + zomapikey;
+    $.ajax({
+        url: zomatourl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // PULL INFO FOR FIRST RESTAURANT
+        $("#res-name0").text(response.restaurants[0].restaurant.name);
+        $("#res-address0").text(response.restaurants[0].restaurant.location.address);
+        $("#res-rating0").text("Rating: " + response.restaurants[0].restaurant.user_rating.aggregate_rating + " (" + response.restaurants[0].restaurant.user_rating.rating_text + ") - " + response.restaurants[0].restaurant.user_rating.votes + " votes");
+        $("#res-url0").attr("href", response.restaurants[0].restaurant.url);
+        $("#res-url0").text("Link");
+        // PULL INFO FOR SECOND RESTAURANT
+        $("#res-name1").text(response.restaurants[1].restaurant.name);
+        $("#res-address1").text(response.restaurants[1].restaurant.location.address);
+        $("#res-rating1").text("Rating: " + response.restaurants[1].restaurant.user_rating.aggregate_rating + " (" + response.restaurants[1].restaurant.user_rating.rating_text + ") - " + response.restaurants[1].restaurant.user_rating.votes + " votes");
+        $("#res-url0").attr("href", response.restaurants[1].restaurant.url);
+        $("#res-url0").html("Link");
+        // PULL INFO FOR THIRD RESTAURANT
+        $("#res-name2").text(response.restaurants[2].restaurant.name);
+        $("#res-address2").text(response.restaurants[2].restaurant.location.address);
+        $("#res-rating2").text("Rating: " + response.restaurants[2].restaurant.user_rating.aggregate_rating + " (" + response.restaurants[2].restaurant.user_rating.rating_text + ") - " + response.restaurants[2].restaurant.user_rating.votes + " votes");
+        $("#res-url0").attr("href", response.restaurants[2].restaurant.url);
+        $("#res-url0").text("Link");
+    })
+})
