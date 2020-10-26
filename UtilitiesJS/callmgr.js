c787f9606cca67b2046d7e793ccb4b5f45025220
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-10-26 00:54:35 +0000

    Push latest update to github

--- UtilitiesJS/callmgr.js
+++ UtilitiesJS/callmgr.js
@@ -22,7 +22,7 @@ function showModal(){
      // <!-- Modal Structure -->
     var div2=$("<div id='modal1' class='modal modal-fixed-footer'>")
       var div3=$("<div class='modal-content'>")
-            //tex structure goes here
+            //text structure goes here
            var h4=$("<h4>Welcome to askKaren!</h4>"); // modal title goes here
            var spc1=$("<p>");// break space
            var txt1=$("<p class='modaltext'> Here, you can search for restaurants near you! But not just any restaurant - only the lowest rated restaurants near you. We know, as a Karen, you expect only the best for you and your family and because of this, we only show you the lowest rated places near you. View the low score, click on the link the view the restaurant’s website and easily click a button to speak with a manager.</p>");
@@ -36,7 +36,7 @@ function showModal(){
            var li3=$("<li>View the top 5 lowest rated restaurants closest to you</li>");
            var li4=$("<li>And click to view more information about that restaurant</li>");
            var dv=$("<div class='modal-footer'>");
-           var aa1=$("<a href='#!' id='yei' class='modal-close waves-effect waves-green btn-flat'>Ok</a>"); // Agree</a>
+           var aa1=$("<a href='#!' id='yei' class='modal-close waves-effect waves-green btn-flat'>This Offends Me</a>"); // Agree</a>
            
             div3.append(h4).append(spc1).append(txt1).append(spc2).append(h7).append(spc3).append(spc4).append(ol1);
             ol1.append(li1).append(li2).append(li3).append(li4);
